c188f138fcb1bac44849c4fa668bc183ace4fb58
kolovsky <f.kolovsky@gmail.com>
2017-10-03 10:11:20 +0200

    add support for relative gap

--- src/main/scala/com/kolovsky/modeler/PathBasedAssignment.scala
+++ src/main/scala/com/kolovsky/modeler/PathBasedAssignment.scala
@@ -4,8 +4,18 @@ import com.kolovsky.graph.Graph
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
-  * Created by kolovsky on 24.5.17.
+  * Solve user equlibrium problem using by Path based algorithm
+  * @param g graph
+  * @param initCost cost edge map without congestion effect
+  * @param cf dependence between traffic volume and cost
+  * @param capacityEdgeMap capacity edge map
+  * @param maxIter maximum number of iteration
+  * @param epsilon maximal relative gap
+  * @param alpha step size for GP algorithm
+  * @param callbackIter callback function, that is call after every iteration with traffic edge map
   */
 class PathBasedAssignment(g: Broadcast[Graph],
                           initCost: Array[Double],
@@ -17,13 +27,18 @@ class PathBasedAssignment(g: Broadcast[Graph],
                           callbackIter: Array[Double] => Unit = _ => ()) extends Assignment with Serializable{
 
   var ps_rdd: RDD[Array[PathSet]] = _
+  var lbs: ArrayBuffer[Double] = ArrayBuffer.empty
+  var info: String = ""
 
+  /**
+    * Perform the algorithm with Origin-Destinatin Matrix
+    * @param odm - Origin-Destinatin Matrix
+    * @return traffic edge map
+    */
   def run(odm: Constant.ROWODM): Array[Double] ={
     // INICIALIZATION
     val paths = odm.map(x => {
-      //val target = x._2.map(t => (t._1.n, t._2))
       val target = x._2.map(t => (t._1.n, t))
-      //g.value.getShortestPathsTrips(x._1.n, target, initCost).map(p => (x._1, p._1, p._2, p._3, p._4))
       g.value.getShortestPathsT(x._1.n, target, initCost).map(p => (x._1, p._3._1, p._2, p._3._2, p._4))
     })
     paths.persist()
@@ -57,12 +72,12 @@ class PathBasedAssignment(g: Broadcast[Graph],
        }))
 
     var cost: Array[Double] = null
+    cost = g.value.edges.map(e => cf.cost(trafficEdgeMap(e.i), capacityEdgeMap(e.i), initCost(e.i)))
     var n = 0
-    var OFo = -1.0 // old value of objective function
     var OF = 0.0 // value of objective function
-    while (n < maxIter && math.abs(OF - OFo) >= epsilon){
+    var rg = 1.0 // value of relative gap
+    while (n < maxIter &&  rg > epsilon){
       // UPDATE
-      cost = g.value.edges.map(e => cf.cost(trafficEdgeMap(e.i), capacityEdgeMap(e.i), initCost(e.i)))
       val after_update = ps_rdd.map(row => row.map(ps => {
         ps.updateCost(cost)
         ps
@@ -101,13 +116,18 @@ class PathBasedAssignment(g: Broadcast[Graph],
 
       //callback
       callbackIter(trafficEdgeMap)
+      // compute cost from traffic
+      cost = g.value.edges.map(e => cf.cost(trafficEdgeMap(e.i), capacityEdgeMap(e.i), initCost(e.i)))
 
       // objective function
-      OFo = OF
       OF = g.value.edges.map(e => {
         cf.integral(trafficEdgeMap(e.i), capacityEdgeMap(e.i), initCost(e.i))
       }).sum
-      println("OF "+n+" = "+math.floor(OF))
+
+      // relative gap
+      rg = relativeGap(OF, trafficEdgeMap, cost, odm)
+      println("RG: "+rg)
+      info += (", "+rg)
 
     }
     trafficEdgeMap
@@ -129,6 +149,19 @@ class PathBasedAssignment(g: Broadcast[Graph],
     ps_rdd.map(_.map(ps => {
       ps.toODCell()
     }))
+  }
+
+  def relativeGap(OF: Double, trafficEdgeMap: Array[Double], costEdgeMap: Array[Double], odm: Constant.ROWODM): Double ={
+    val aon = new AllOrNothingAssignment(g, costEdgeMap)
+    val aon_trafficEdgeMap = aon.run(odm)
+    var gap: Double = 0
+    for (i <- trafficEdgeMap.indices){
+      gap += costEdgeMap(i) * (aon_trafficEdgeMap(i) - trafficEdgeMap(i))
+    }
+    lbs += (OF + gap)
+    val blb = lbs.max
+    val rg = - gap / math.abs(blb)
+    rg
   }
 
 }
